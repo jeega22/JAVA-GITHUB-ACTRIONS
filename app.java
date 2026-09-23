72e14794770522539bddcfaa3ca4477f90c1d2db
ajith <ajithjeega2@gmail.com>
2026-09-23 15:59:03 +0000

    java-code

---
+++ app.java
@@ -0,0 +1,18 @@
+public class app {
+	public static void main(string[] args) {
+		int eno=1001;
+		string ename="ajith"
+			int sal=10000
+			string course="Devops"
+			system.out.println("Learn here");
+		     	system.out.println("My emp no:" +eno);
+			system.out.println("My name is:"ename);  
+			system.out.println("My salary is:" +sal);
+			system.out.println("My course is:" +course);
+			system.out.println("Thanks");
+	}
+}
+		        
+		        
+		        
+		        
