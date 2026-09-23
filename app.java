0d9d85ecb9a6b51c9037fc6e4f303656736c424e
jeega22 <ajithjeega22@gmail.com>
2026-09-23 21:54:10 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,18 +1,14 @@
-public class app {
-	public static void main(string[] args) {
-		int eno=1001;
-		string ename="ajith"
-			int sal=10000
-			string course="Devops"
-			system.out.println("Learn here");
-		     	system.out.println("My emp no:" +eno);
-			system.out.println("My name is:"ename);  
-			system.out.println("My salary is:" +sal);
-			system.out.println("My course is:" +course);
-			system.out.println("Thanks");
-	}
+	public class app {
+    public static void main(String[] args) {
+        String ename = "ajith";
+        int sal = 10000;
+        String course = "Devops";
+
+        System.out.println("My name is: " + ename);
+        System.out.println("Salary: " + sal);
+        System.out.println("Course: " + course);
+    }
 }
-		        
-		        
+	        
 		        
 		        
